7606f0b72cfa1ef9439228be178fa5dff6ab71a6
Diego2211 <diegoleandropalacios04@gmail.com>
2026-03-23 01:21:44 -0300

    creacion de app core y implementacion en settings.py

--- GymManager/urls.py
+++ GymManager/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("miembros.urls")),
-    path("logeo/", include("accounts.urls"))
+    path("logeo/", include("accounts.urls")),
+    path("", include("core.urls"))
 
 ]
