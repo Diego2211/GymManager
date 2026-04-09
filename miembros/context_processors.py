44bb567ee2064c99ddc658d82573e7976dc3fb3d
Diego2211 <diegoleandropalacios04@gmail.com>
2026-04-09 02:11:17 -0300

    gym

--- miembros/context_processors.py
+++ miembros/context_processors.py
@@ -3,3 +3,8 @@ def nombre_usuario(request):
     if request.user.is_authenticated and hasattr(request, 'nombre'):
         return {'nombre_usuario': request.nombre}
     return {'nombre_usuario': ''}
+
+def nombre_gym(request):
+    if request.user.is_authenticated and hasattr(request, 'gym'):
+        return{'nombre_gym': request.gym}
+    return {'nombre_gym': ''}
